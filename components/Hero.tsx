9c2a157cbecfcbe4ff376ab134450649695ee421
Kaushall-raut <KaushalRaut.code@gmail.com>
2024-11-30 17:47:30 +0530

    added spotlight effect in component using acertinity ui

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -1,9 +1,13 @@
 import React from "react";
+import { Spotlight } from "./ui/Spotlight";
 
 const Hero = () => {
   return (
     <div className="pb-20 pt-36 ">
-      <div className="text-white">Hello world</div>
+      <div><Spotlight className="-top-40 -left-10 md:-left-32 md:-top-20 h-screen" fill="white"/>
+      <Spotlight className="-top-10 left-full md:-left-32 h-[80vh] w-[50vw]" fill="purple"/>
+      <Spotlight className="top-28 left-80 md:-left-32 h-[80vh] w-[vw]" fill="blue"/>
+      </div>
     </div>
   );
 };
---
+++ lib/util.ts
@@ -0,0 +1,7 @@
+// Helper function for adding spotlight effect
+import { ClassValue, clsx } from "clsx";
+import { twMerge } from "tailwind-merge";
+
+export function cn(...inputs: ClassValue[]) {
+  return twMerge(clsx(inputs));
+}
