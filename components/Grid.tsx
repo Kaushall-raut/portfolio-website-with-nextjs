d80faed2f61a99dd58e97783ff58caebb4995fa3
Kaushall-raut <KaushalRaut.code@gmail.com>
2024-12-01 22:29:47 +0530

    added folder to add project data

--- components/Grid.tsx
+++ components/Grid.tsx
@@ -1,10 +1,11 @@
+import { GridData } from "@/data";
 import { BentoGrid, BentoGridItem } from "./ui/Bentogrid";
 
 export const Grid = () => {
   return (
     <section id="about">
       <BentoGrid>
-        {[{title: "Title1", description: "desc1"}].map((item) => (
+        {GridData.map((item) => (
           <BentoGridItem id={item.id} key={item.id} title={item.title} description={item.description} />
         ))}
       </BentoGrid>
---
+++ data/index.ts
@@ -0,0 +1,4 @@
+export const GridData=
+    [{title: "project 1", description: "desc1",id:1},
+    {title: "project 2", description: "description",id:2},
+    {title: "project 3", description: "description",id:3}]
